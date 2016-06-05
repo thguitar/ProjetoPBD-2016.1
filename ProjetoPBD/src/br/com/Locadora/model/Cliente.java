d297a276c2d11308aa40c0147f358acdd9fe2a44
Alan Limeira <alanlimeira@gmail.com>
2016-06-05 14:45:12 -0300

    Adcionado os atributos da classe Cliente com seus gets e sets.

--- ProjetoPBD/src/br/com/Locadora/model/Cliente.java
+++ ProjetoPBD/src/br/com/Locadora/model/Cliente.java
@@ -2,5 +2,76 @@ package br.com.Locadora.model;
 
 abstract class Cliente {
 	
+	private int id, numero;
+	private String nome, rua, bairro, cidade, estado;
+	
+	public Cliente(int id, int numero, String nome, String rua, String bairro,
+			String cidade, String estado) {
+		this.id = id;
+		this.numero = numero;
+		this.nome = nome;
+		this.rua = rua;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 
 }
