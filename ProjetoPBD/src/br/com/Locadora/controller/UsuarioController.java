78488c6ec9929a86433af539ef2a706415c3b002
Alan Limeira <alanlimeira@gmail.com>
2016-06-19 11:47:54 -0300

    Criado metodo de usca por id em usuario controle

--- ProjetoPBD/src/br/com/Locadora/controller/UsuarioController.java
+++ ProjetoPBD/src/br/com/Locadora/controller/UsuarioController.java
@@ -17,6 +17,15 @@ public class UsuarioController {
 		emf = Persistence.createEntityManagerFactory("Brito");
 		em = emf.createEntityManager();
 	}
+	
+	public Usuario consultaId(int id){
+		em.getTransaction().begin();
+		Usuario usuario = em.find(Usuario.class, id);
+		em.getTransaction().commit();
+		em.close();
+		return usuario;
+	}
+	
 	public void salvar(Usuario u){
 		em.getTransaction().begin();
 		em.merge(u);
