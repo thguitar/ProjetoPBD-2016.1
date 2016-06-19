a74d75df7064b70ff865480188c4b7c667187c8a
Alan Limeira <alanlimeira@gmail.com>
2016-06-19 14:54:45 -0300

    Criada tela para consulta de usuarios

--- ProjetoPBD/src/br/com/Locadora/main/Main.java
+++ ProjetoPBD/src/br/com/Locadora/main/Main.java
@@ -3,14 +3,16 @@ package br.com.Locadora.main;
 
 import br.com.Locadora.controller.LoginController;
 import br.com.Locadora.view.TelaCadastroUsuario;
+import br.com.Locadora.view.TelaConsultaUsuarios;
 import br.com.Locadora.view.TelaLogin;
 
 public class Main {
 
 	public static void main(String[] args) {
-		TelaLogin telaLogin = new TelaLogin();
-		LoginController controller = new LoginController(telaLogin);
-		controller.controll();
+		new TelaConsultaUsuarios().setVisible(true);
+//		TelaLogin telaLogin = new TelaLogin();
+//		LoginController controller = new LoginController(telaLogin);
+//		controller.controll();
 	}	
 
 }
