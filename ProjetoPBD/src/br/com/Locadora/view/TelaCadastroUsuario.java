258750b8a2d5380fee43e14d5a7620b854132ae5
Thiago <thiagol.guitarx@gmail.com>
2016-06-20 12:10:44 -0300

    Alteração do Layout na Tela Cadastro de Usuário e criação do metodod para validar se os campos estão em branco.

--- ProjetoPBD/src/br/com/Locadora/view/TelaCadastroUsuario.java
+++ ProjetoPBD/src/br/com/Locadora/view/TelaCadastroUsuario.java
@@ -1,6 +1,5 @@
 package br.com.Locadora.view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -15,8 +14,8 @@ import java.awt.Font;
 import java.awt.Color;
 
 import javax.swing.JComboBox;
-import javax.swing.JRadioButton;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import br.com.Locadora.controller.UsuarioController;
@@ -25,16 +24,35 @@ import br.com.Locadora.model.Usuario;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import javax.swing.JPasswordField;
+import javax.swing.JCheckBox;
+
 public class TelaCadastroUsuario extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField nomeField;
-	private JTextField emailField;
-	private JTextField senhaField;
+	private static final long serialVersionUID = -7442135931771402780L;
 
-	/**
-	 * Launch the application.
-	 */
+	private JPanel contentPane;
+	private JPanel panelTitulo;
+	private JPanel panelCentro;
+	private JPanel panelRodape;
+	private JLabel labelTitulo;
+	private JLabel labelCodigo;
+	private JLabel labelEmail;
+	private JLabel labelSenha;
+	private JLabel labelEmpresa;
+	private JLabel labelLogin;
+	private JLabel labelNome;
+	private JTextField fieldCodigo;
+	private JTextField fieldNome;
+	private JTextField fieldEmail;
+	private JTextField fieldLogin;
+	private JPasswordField passFieldSenha;
+	@SuppressWarnings("rawtypes")
+	private JComboBox comboBoxEmpresa;
+	private JCheckBox chckbxAdmin;
+	private JButton buttonCadastrar;
+	private JButton buttonCancelar;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -48,9 +66,8 @@ public class TelaCadastroUsuario extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
+	@SuppressWarnings("rawtypes")
 	public TelaCadastroUsuario() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,105 +76,129 @@ public class TelaCadastroUsuario extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 507, 57);
-		panel.setBackground(SystemColor.inactiveCaption);
-		contentPane.add(panel);
-		
-		JLabel lblCadastroDeUsurios = new JLabel("Cadastro de Usu\u00E1rios");
-		lblCadastroDeUsurios.setForeground(new Color(0, 0, 102));
-		lblCadastroDeUsurios.setFont(new Font("SansSerif", Font.BOLD, 32));
-		panel.add(lblCadastroDeUsurios);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(5, 308, 497, 1);
-		panel_1.setBackground(SystemColor.inactiveCaption);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel label = new JLabel("Cadastro de Usu\u00E1rios");
-		label.setBounds(83, 5, 331, 42);
-		label.setForeground(new Color(0, 0, 102));
-		label.setFont(new Font("SansSerif", Font.BOLD, 32));
-		panel_1.add(label);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(SystemColor.inactiveCaption);
-		panel_2.setBounds(0, 292, 507, 17);
-		contentPane.add(panel_2);
-		
-		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setFont(new Font("SansSerif", Font.BOLD, 12));
-		lblNome.setBounds(10, 68, 47, 16);
-		contentPane.add(lblNome);
-		
-		JLabel lblEmail = new JLabel("E-mail:");
-		lblEmail.setFont(new Font("SansSerif", Font.BOLD, 12));
-		lblEmail.setBounds(10, 112, 47, 16);
-		contentPane.add(lblEmail);
-		
-		JLabel lblSenha = new JLabel("Senha:");
-		lblSenha.setFont(new Font("SansSerif", Font.BOLD, 12));
-		lblSenha.setBounds(10, 156, 47, 16);
-		contentPane.add(lblSenha);
-		
-		JLabel lblEmpresa = new JLabel("Empresa:");
-		lblEmpresa.setFont(new Font("SansSerif", Font.BOLD, 12));
-		lblEmpresa.setBounds(272, 68, 63, 16);
-		contentPane.add(lblEmpresa);
-		
-		JComboBox empresaComboBox = new JComboBox();
-		empresaComboBox.setBounds(345, 68, 134, 20);
-		contentPane.add(empresaComboBox);
-		
-		JRadioButton adminRadioButton = new JRadioButton("Administrador");
-		adminRadioButton.setFont(new Font("SansSerif", Font.BOLD, 12));
-		adminRadioButton.setBounds(273, 109, 109, 23);
-		contentPane.add(adminRadioButton);
-		
-		JButton button = new JButton("Cadastrar");
-		button.addActionListener(new ActionListener() {
+
+		panelTitulo = new JPanel();
+		panelTitulo.setBounds(0, 0, 507, 57);
+		panelTitulo.setBackground(SystemColor.inactiveCaption);
+		contentPane.add(panelTitulo);
+
+		labelTitulo = new JLabel("Cadastro de Usu\u00E1rios");
+		labelTitulo.setForeground(new Color(0, 0, 102));
+		labelTitulo.setFont(new Font("SansSerif", Font.BOLD, 32));
+		panelTitulo.add(labelTitulo);
+
+		panelCentro = new JPanel();
+		panelCentro.setBounds(33, 69, 441, 190);
+		panelCentro.setBackground(SystemColor.inactiveCaption);
+		contentPane.add(panelCentro);
+		panelCentro.setLayout(null);
+
+		labelEmail = new JLabel("E-mail:");
+		labelEmail.setBounds(67, 82, 38, 16);
+		panelCentro.add(labelEmail);
+		labelEmail.setFont(new Font("SansSerif", Font.BOLD, 12));
+
+		labelSenha = new JLabel("Senha:");
+		labelSenha.setBounds(258, 58, 39, 16);
+		panelCentro.add(labelSenha);
+		labelSenha.setFont(new Font("SansSerif", Font.BOLD, 12));
+
+		labelEmpresa = new JLabel("Empresa:");
+		labelEmpresa.setBounds(51, 106, 54, 16);
+		panelCentro.add(labelEmpresa);
+		labelEmpresa.setFont(new Font("SansSerif", Font.BOLD, 12));
+
+		comboBoxEmpresa = new JComboBox();
+		comboBoxEmpresa.setBounds(109, 104, 138, 20);
+		panelCentro.add(comboBoxEmpresa);
+
+		fieldLogin = new JTextField();
+		fieldLogin.setBounds(109, 56, 114, 20);
+		panelCentro.add(fieldLogin);
+		fieldLogin.setColumns(10);
+
+		labelLogin = new JLabel("Nome (Login):");
+		labelLogin.setBounds(27, 58, 78, 16);
+		panelCentro.add(labelLogin);
+
+		labelNome = new JLabel("Nome Completo:");
+		labelNome.setBounds(12, 34, 93, 16);
+		panelCentro.add(labelNome);
+		labelNome.setFont(new Font("SansSerif", Font.BOLD, 12));
+
+		fieldNome = new JTextField();
+		fieldNome.setBounds(109, 32, 306, 20);
+		panelCentro.add(fieldNome);
+		fieldNome.setColumns(10);
+
+		fieldEmail = new JTextField();
+		fieldEmail.setBounds(109, 80, 250, 20);
+		panelCentro.add(fieldEmail);
+		fieldEmail.setColumns(10);
+
+		passFieldSenha = new JPasswordField();
+		passFieldSenha.setBounds(301, 56, 114, 20);
+		panelCentro.add(passFieldSenha);
+
+		chckbxAdmin = new JCheckBox("Administrador");
+		chckbxAdmin.setBounds(304, 102, 112, 24);
+		panelCentro.add(chckbxAdmin);
+
+		labelCodigo = new JLabel("C\u00F3digo:");
+		labelCodigo.setBounds(63, 10, 42, 16);
+		panelCentro.add(labelCodigo);
+
+		fieldCodigo = new JTextField();
+		fieldCodigo.setEditable(false);
+		fieldCodigo.setBounds(109, 8, 45, 20);
+		panelCentro.add(fieldCodigo);
+		fieldCodigo.setColumns(10);
+
+		panelRodape = new JPanel();
+		panelRodape.setBackground(SystemColor.inactiveCaption);
+		panelRodape.setBounds(0, 292, 507, 17);
+		contentPane.add(panelRodape);
+
+
+		buttonCadastrar = new JButton("Cadastrar");
+		buttonCadastrar.setForeground(new Color(0, 102, 0));
+		buttonCadastrar.setFont(new Font("SansSerif", Font.BOLD, 13));
+		buttonCadastrar.setBackground(new Color(204, 255, 255));
+		buttonCadastrar.setBounds(93, 269, 105, 23);
+		contentPane.add(buttonCadastrar);
+		buttonCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Usuario u = new Usuario();
-				u.setNome(nomeField.getText());
-				u.setSenha(senhaField.getText());
-				u.setEmail(emailField.getText());
-				u.setAdmin(adminRadioButton.isSelected());
-				new UsuarioController().salvar(u);
-			}
+				if (validarFields()) {
+					Usuario u = new Usuario();
+					u.setNome(fieldNome.getText());
+					u.setSenha(fieldLogin.getText());
+					u.setEmail(fieldEmail.getText());
+					u.setAdmin(chckbxAdmin.isSelected());
+					new UsuarioController().salvar(u);
+				}
+			}	
 		});
-		button.setForeground(new Color(0, 102, 0));
-		button.setFont(new Font("SansSerif", Font.BOLD, 13));
-		button.setBackground(new Color(204, 255, 255));
-		button.setBounds(130, 217, 105, 23);
-		contentPane.add(button);
-		
-		JButton button_1 = new JButton("Cancelar");
-		button_1.addActionListener(new ActionListener() {
+
+		buttonCancelar = new JButton("Cancelar");
+		buttonCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
-		button_1.setForeground(Color.RED);
-		button_1.setFont(new Font("SansSerif", Font.BOLD, 13));
-		button_1.setBackground(new Color(255, 204, 255));
-		button_1.setBounds(277, 218, 105, 23);
-		contentPane.add(button_1);
-		
-		nomeField = new JTextField();
-		nomeField.setBounds(67, 67, 200, 20);
-		contentPane.add(nomeField);
-		nomeField.setColumns(10);
-		
-		emailField = new JTextField();
-		emailField.setColumns(10);
-		emailField.setBounds(67, 111, 200, 20);
-		contentPane.add(emailField);
-		
-		senhaField = new JTextField();
-		senhaField.setColumns(10);
-		senhaField.setBounds(67, 155, 105, 20);
-		contentPane.add(senhaField);
+		buttonCancelar.setForeground(Color.RED);
+		buttonCancelar.setFont(new Font("SansSerif", Font.BOLD, 13));
+		buttonCancelar.setBackground(new Color(255, 204, 255));
+		buttonCancelar.setBounds(275, 269, 105, 23);
+		contentPane.add(buttonCancelar);
+	}
+
+	@SuppressWarnings("deprecation")
+	private boolean validarFields(){
+		if (fieldNome.getText().isEmpty()||fieldEmail.getText().isEmpty()||fieldLogin.getText().isEmpty()||passFieldSenha.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Campos Obrigatórios", null, JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+
+		return true;
 	}
 }
