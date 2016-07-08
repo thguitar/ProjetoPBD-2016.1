9c923ce23b36f3762e5b0a01a70ed8aba16d86f1
Thiago <thiagol.guitarx@gmail.com>
2016-07-08 15:41:47 -0300

    Adição da Classe Configuracoes

---
+++ ProjetoPBD/src/br/com/Locadora/model/Configuracoes.java
@@ -0,0 +1,29 @@
+package br.com.Locadora.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONFIGURACOES")
+public class Configuracoes {
+
+	private String pathPlanodeFundo; 
+
+
+	public Configuracoes(){
+
+	}
+
+	public Configuracoes(String pathPlanodeFundo){
+		this.pathPlanodeFundo = pathPlanodeFundo;
+	}
+
+	public String getPathPlanodeFundo() {
+		return pathPlanodeFundo;
+	}
+
+	public void setPathPlanodeFundo(String pathPlanodeFundo) {
+		this.pathPlanodeFundo = pathPlanodeFundo;
+	}
+	
+}
