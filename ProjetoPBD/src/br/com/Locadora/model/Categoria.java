088c040d35d2c87d6259ebb4bceabffa1b5c814b
Thiago <thiagol.guitarx@gmail.com>
2016-07-08 15:31:18 -0300

    Alteração dos Atributos e Anotações da Classe Categoria

--- ProjetoPBD/src/br/com/Locadora/model/Categoria.java
+++ ProjetoPBD/src/br/com/Locadora/model/Categoria.java
@@ -2,16 +2,12 @@ package br.com.Locadora.model;
 
 
 
-import java.util.Calendar;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 public class Categoria {
@@ -20,17 +16,8 @@ public class Categoria {
 	private int ID;
 	@Column(name = "DESCRICAO", nullable = false)
 	private String descricao;
-	@Column(name = "HRS_RESERVADAS_INICIO", nullable = false)
-	@Temporal(TemporalType.TIME)
-	private Date horasReservadasInicio;
-	@Column(name = "HRS_RESERVADAS_FIM", nullable = false)
-	@Temporal(TemporalType.TIME)
-	private Date horasReservadasFim;
-	@Column(name = "DT_RESERVA", nullable = false)
-	@Temporal(TemporalType.DATE)
-	private Calendar dataReservada;
-	@Column(name = "KM_PARA_REVISAO", nullable = false)
-	private int kmParaRevisao;
+	@Column(name = "VALOR_ALUGUEL", nullable = false)
+	private double valorAluguel;
 
 	public Categoria(){
 		
@@ -51,38 +38,5 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
-	public Date getHorasReservadasInicio() {
-		return horasReservadasInicio;
-	}
-
-	public void setHorasReservadasInicio(Date horasReservadasInicio) {
-		this.horasReservadasInicio = horasReservadasInicio;
-	}
-
-	public Date getHorasReservadasFim() {
-		return horasReservadasFim;
-	}
-
-	public void setHorasReservadasFim(Date horasReservadasFim) {
-		this.horasReservadasFim = horasReservadasFim;
-	}
-
-	public Calendar getDataReservada() {
-		return dataReservada;
-	}
-
-	public void setDataReservada(Calendar dataReservada) {
-		this.dataReservada = dataReservada;
-	}
-
-	public int getKmParaRevisao() {
-		return kmParaRevisao;
-	}
-
-	public void setKmParaRevisao(int kmParaRevisao) {
-		this.kmParaRevisao = kmParaRevisao;
-	}
-	
 
 }
